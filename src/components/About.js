b63a07df52341c90eb7d8969cd9eeb247b035f04
oznuren <95101906+oznuren@users.noreply.github.com>
2022-07-05 13:28:29 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+const bioHandler = (bio) => {
+  if (bio) {
+    return <p>{bio}</p>;
+  }
+};
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioHandler(props.bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links linkedin={props.linkedin} github={props.github} />
     </div>
   );
 }
-
 export default About;
